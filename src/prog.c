38cf20cbe623f403ca7ebe2c341318441a363af3
Carlos Pontón Valladares <carlosdaniel.pontonv@um.es>
2020-04-02 17:23:07 +0200

    añadida funcion nueva

--- src/prog.c
+++ src/prog.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+void cuenta1aN(int n){
+	int i;
+	for (i = 0; i < n; i++)
+		printf("%d\n", i+1);
+}
+
 int main() {
 	printf("Hola Mundo!");
+	int n = 10;
+	cuenta1aN(n);
+	return 0;
 }
